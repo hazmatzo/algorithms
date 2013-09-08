15c2698434d5bf741668541f035b2ef65af836a9
Zoe Madden-Wood <maddenwood@gmail.com>
2013-09-07 23:31:47 -0700

    Refactored the Fizzbuzz problem
    Updated the Fizzbuzz problem in Javascript to work and then refactored
    it to make it better.

--- Random Problems/FizzBuzz/Fizzbuzz.js
+++ Random Problems/FizzBuzz/Fizzbuzz.js
@@ -1,35 +1,40 @@
 // Print all numbers under 100, if the number is divisible by 3, print "Fizz,"
 // if the number is divisible by 5, print "Buzz," if it's divisible by both,
-// print "FizzBuzz". First attempt in Javascript.
+// print "FizzBuzz". 
 // Zoe Madden-Wood
 
-var Fizzbuzz = [1];
-
-var Fizz(testInteger) { 
-  var remainder = testInteger / 3;
-    if (remainder === 0)
-        return true
+// Checks to see if number is divisible by 3.
+function Fizz(testInteger) {
+  var remainder = testInteger % 3;
+    if (remainder === 0) {
+        return true;
+      }
+    return false;
 }
 
-var Buzz(testInteger) {
-  var remainder = testInteger / 5;
-    if (remainder === 0)
-    	return true
+// Checks to see if number is divisible by 5.
+function Buzz(testInteger) {
+  var remainder = testInteger % 5;
+    if (remainder === 0) {
+      return true;
+    }
+    return false;
 }
 
-var testInteger {
-  Array[Array.length-1] + 1
-}
+var fizzbuzz = 1;
 
-  while (Fizzbuzz[Fizzbuzz.length-1] <= 100) {
-    (if Fizz && Buzz
-    	Fizzbuzz << "Fizzbuzz")
-    (else if Fizz 
-  	    Fizzbuzz << "Fizz")
-    else if Buzz
-    	Fizzbuzz << "Buzz"
-    else 
-  	    Fizzbuzz << testInteger
+  while (fizzbuzz < 100) {
+    if ((Fizz(fizzbuzz)) && (Buzz(fizzbuzz))) {
+      console.log("FizzBuzz");
+    }
+    else if (Fizz(fizzbuzz)) {
+      console.log("Fizz");
+    }
+    else if (Buzz(fizzbuzz)) {
+      console.log("Buzz");
+    }
+    else {
+      console.log(fizzbuzz);
+    }
+    fizzbuzz = fizzbuzz + 1;
   }
-
-console.log(Fizzbuzz)
