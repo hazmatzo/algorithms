7d49a895ae4b9d8fe5b823483439b94476a597df
Zoe Madden-Wood <maddenwood@gmail.com>
2014-05-26 09:03:35 -0700

    Cleaned up folders by deleting old problems.

--- Random Problems/Print-Sixes/ZoeMWprintsixes.js
+++
@@ -1,7 +0,0 @@
-var printsixes = [6];
-while (printsixes[printsixes.length-1] < 95) {
-	i = i + 6;
-}
-  printsixes.push(i)
-
-console.log(printsixes)
--- Random Problems/Print-Sixes/printsixes3.rb
+++
@@ -1,3 +0,0 @@
-(6..100).step(6) do |printsixes|
-    print printsixes, "\n"
-end
